0ac092e207aa8bad9eed7a8c0e165d61cab13a3c
Julien Catania <julien.catania@gmail.com>
2022-08-28 22:58:32 +0200

    algo validator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, TextField} from '@mui/material';
+import {useState} from 'react';
+
+function isAlgoWorking(algo) {
+    const splited = algo.split('');
+    let result = '';
+    let i = 0
+    let incorrect = false;
+
+    while (splited.length > 0) {
+        if (i % 2 === 1) {
+            result += splited.shift();
+        } else {
+            splited.push(splited.shift())
+        }
+        i++;
+    }
+
+    for (let j = 0; j < result.length; j += 2) {
+        incorrect = incorrect || result[j] === result [j+1];
+    }
+
+    console.log(`%cAlgo : ${algo} ${incorrect ? 'KO' : 'OK'}`, `color: ${incorrect ? 'red' : 'green'}`)
+
+    return !incorrect;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [algo, setAlgo] = useState('RNNRNNRR');
+    const [correct, setCorrect] = useState();
+
+    return (
+        <div className="App">
+            <TextField id="outlined-basic" label="Algorithme" variant="outlined"
+                       onChange={e => setAlgo(e.target.value)} defaultValue="RNNRNNRR"/>
+            <Button variant="contained" onClick={_ => setCorrect(isAlgoWorking(algo))}>Tester !</Button>
+            {correct !== undefined ? (correct === true ? '💪' : '😩') : ''}
+        </div>
+    );
 }
 
 export default App;
