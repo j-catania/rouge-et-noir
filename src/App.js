b5bcd79afa62f18e5aeb9e97db80ae38816a4f3a
Julien Catania <julien.catania@gmail.com>
2022-08-30 22:32:33 +0200

    IHM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import {Button, TextField} from '@mui/material';
+import {Alert, Button, Snackbar, TextField, ToggleButton, ToggleButtonGroup} from '@mui/material';
 import {useState} from 'react';
 
-function isAlgoWorking(algo) {
+function isAlgoCorrect(algo) {
     const splited = algo.split('');
     let result = '';
     let i = 0
@@ -18,7 +18,7 @@ function isAlgoWorking(algo) {
     }
 
     for (let j = 0; j < result.length; j += 2) {
-        incorrect = incorrect || result[j] === result[j+1];
+        incorrect = incorrect || result[j] === result[j + 1];
     }
 
     console.log(`%cAlgo : ${algo} ${incorrect ? 'KO' : 'OK'}`, `color: ${incorrect ? 'red' : 'green'}`)
@@ -26,18 +26,91 @@ function isAlgoWorking(algo) {
     return !incorrect;
 }
 
+function translateNumbersToAlgo(numbers) {
+    if (!numbers) {
+        return '';
+    }
+    const splittedNumbers = numbers.split(' ');
+    const chars = ['R', 'N'];
+
+    let isFirstCharToPick = true;
+    let rt = '';
+
+    while (splittedNumbers.length > 0) {
+        const count = splittedNumbers.shift();
+        for (let i = 0; i < count; i++) {
+            rt += chars[isFirstCharToPick ? 0 : 1]
+        }
+        isFirstCharToPick = !isFirstCharToPick;
+    }
+
+    return rt;
+}
+
 function App() {
-    const [algo, setAlgo] = useState('RNNRNNRR');
+    const [open, setOpen] = useState(false);
+    const [data, setData] = useState('');
     const [correct, setCorrect] = useState();
+    const [type, setType] = useState('algo');
 
     return (
         <div className="App">
-            <TextField id="outlined-basic" label="Algorithme" variant="outlined"
-                       onChange={e => setAlgo(e.target.value)} defaultValue="RNNRNNRR"/>
-            <Button variant="contained" onClick={_ => setCorrect(isAlgoWorking(algo))}>Tester !</Button>
-            {correct !== undefined ? (correct === true ? '💪' : '😩') : ''}
+            <div className="form">
+                <ToggleButtonGroup
+                    value={type}
+                    exclusive
+                    onChange={(e, newValue) => setType(newValue)}
+                    aria-label="text alignment"
+                >
+                    <ToggleButton value="algo" aria-label="left aligned">
+                        Algorithme
+                    </ToggleButton>
+                    <ToggleButton value="numero" aria-label="left aligned">
+                        Numéros
+                    </ToggleButton>
+                </ToggleButtonGroup>
+
+                <TextField label={type === 'algo' ? 'Algorithme' : 'Numéros'}
+                           variant="outlined"
+                           value={data}
+                           onChange={e => {
+                               setData(e.target.value)
+                           }}/>
+            </div>
+
+            <div className="form">
+                <Button variant="contained"
+                        onClick={_ => {
+                            let info = '';
+                            if (type === 'algo') {
+                                info = data;
+                            } else {
+                                info = translateNumbersToAlgo(data);
+                            }
+                            setCorrect(isAlgoCorrect(info))
+                            setOpen(true);
+                        }}
+                        disabled={!data}>Vérifier</Button>
+
+                <Button variant="text" onClick={_ => {
+                    setCorrect(undefined);
+                    setData('');
+                    setType('algo')
+                }}>Réinitialiser</Button>
+            </div>
+
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+                onClose={_ => setOpen(false)}
+            >
+                <Alert
+                    severity={correct ? 'success' : 'error'}>{correct ? 'Bravo 💪' : 'Ça ne correspond pas 😩'}</Alert>
+            </Snackbar>
         </div>
     );
 }
 
 export default App;
+export {isAlgoCorrect, translateNumbersToAlgo};
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen} from '@testing-library/react';
+import App, {isAlgoCorrect, translateNumbersToAlgo} from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App/>);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
+
+test('if algo is correct', () => {
+    const rt = isAlgoCorrect('RNNRNNRR')
+    expect(rt).toBeTruthy();
+})
+test('if algo is incorrect', () => {
+    const rt = isAlgoCorrect('RNNRNNRRR')
+    expect(rt).toBeFalsy();
+})
+
+test('translate 1 2 1 2 2', () => {
+    const rt = translateNumbersToAlgo('1 2 1 2 2')
+    expect(rt).toBe('RNNRNNRR');
+})
+
