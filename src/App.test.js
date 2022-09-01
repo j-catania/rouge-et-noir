6bee90b68c176ab197232177fbfbd4c31ebdc2b1
Julien Catania <julien.catania@gmail.com>
2022-09-01 12:06:37 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 import {render, screen} from '@testing-library/react';
 import App, {isAlgoCorrect, translateNumbersToAlgo, findAlgo} from './App';
 
-test('renders learn react link', () => {
+/*test('renders learn react link', () => {
     render(<App/>);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
-});
+});*/
 
 test('if algo is correct', () => {
     const rt = isAlgoCorrect('RNNRNNRR')
