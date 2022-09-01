ef7871454a62d2ab9c5270a641578428afbc14c2
Julien Catania <julien.catania@gmail.com>
2022-09-01 12:15:42 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import {render, screen} from '@testing-library/react';
-import App, {isAlgoCorrect, translateNumbersToAlgo, findAlgo} from './App';
+import App, {isAlgoCorrect, translateNumbersToAlgo, findAlgo, manipule} from './App';
 
 /*test('renders learn react link', () => {
     render(<App/>);
@@ -8,20 +8,26 @@ import App, {isAlgoCorrect, translateNumbersToAlgo, findAlgo} from './App';
 });*/
 
 test('if algo is correct', () => {
-    const rt = isAlgoCorrect('RNNRNNRR')
+    const rt = isAlgoCorrect('RNNRNNRR');
     expect(rt).toBeTruthy();
-})
+});
+
 test('if algo is incorrect', () => {
-    const rt = isAlgoCorrect('RNNRNNRRR')
+    const rt = isAlgoCorrect('RNNRNNRRR');
     expect(rt).toBeFalsy();
-})
+});
 
 test('translate 1 2 1 2 2', () => {
     const rt = translateNumbersToAlgo('1 2 1 2 2')
     expect(rt).toBe('RNNRNNRR');
-})
+});
+
+test('manipule 12345678', () => {
+    const rt = manipule('12345678');
+    expect(rt).toBe('24683751');
+});
 
 test('find algo for 8', () => {
-    const rt = findAlgo(8)
+    const rt = findAlgo(8);
     expect(rt).toBe('RNRNRNRN');
-})
+});
